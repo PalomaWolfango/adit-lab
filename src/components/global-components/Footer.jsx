d7b80a5f7ac01c898dcf019f102f42beebdeebb7
PalomaWolfango <paloma.wolfango@gmail.com>
2021-04-25 16:12:39 +0100

    Contact us on footer (improvement)

--- src/components/global-components/Footer.jsx
+++ src/components/global-components/Footer.jsx
@@ -13,68 +13,18 @@ const Footer =()=>{
                         {/* <!-- start footer-top area --> */}
                         <section className="footer-top">
                             <div className="container">
-                                <div className="row">
+                                <div className="row col-120">
                                         <img src={data.logos.logo1}/>
                                         <img src={data.logos.logo2}/>
-                                        <p>
-                                            <Link  to="/contact">
-                                                <p><b>{data.contactUs}</b></p>
-                                            </Link>
-                                            <p>{data.phone}</p>
-                                            <p>{data.email}</p>
-                                        </p>
-                                        {/* <p>
-                                        {data.mainFooter.about.content}
-                                        </p>
-                                        <div className="more">
-                                            <Link to="/about">read more <i className="flaticon-chevron-1"></i></Link>
+                                        <div className="contacts col-3">
+                                            <p>
+                                                <Link  to="/contact">
+                                                    <p><b>{data.contactUs}</b></p>
+                                                </Link>
+                                                <p>{data.titlePhone} {data.phoneNumber}</p>
+                                                <p>{data.titleEmail} {data.email}</p>
+                                            </p>
                                         </div>
-                                        <div className="social">
-                                            <ul className="d-flex">
-                                                {data.mainFooter.about.social.map((item, i)=>{
-                                                    return <li key={i}><Link to={item.idLink}><i className={`fab ${item.icon}`}></i></Link></li>
-                                                })}
-                                            </ul>
-                                        </div> */}
-                                    {/* <div className="col-xl-3 col-lg-3 col-md-6 mt-sm-30">
-                                        <div className="title">
-                                            <h4>{data.mainFooter.info.title}</h4>
-                                        </div>
-                                        <div className="info">
-                                            <ul>
-                                                {data.mainFooter.info.pageLink.map((item, i)=>{
-                                                    return <li key={i}><Link to={item.pageLink}>{item.pageName}</Link></li>
-                                                })}
-                                            </ul>
-                                        </div>
-                                    </div> */}
-                                    {/* <div className="col-xl-2 col-lg-3 col-md-6 mt-md-30">
-                                        <div className="title">
-                                            <h4>{data.mainFooter.services.title}</h4>
-                                        </div>
-                                        <div className="service-list">
-                                            <ul>
-                                            {data.mainFooter.services.pageLink.map((item, i)=>{
-                                                    return <li key={i}><Link to={item.pageLink}>{item.pageName}</Link></li>
-                                                })}
-                                            </ul>
-                                        </div>
-                                    </div> */}
-                                    {/* <div className="col-xl-3 col-lg-3 col-md-6 mt-md-30">
-                                        <div className="title">
-                                            <h4>{data.mainFooter.newsLetter.title}</h4>
-                                        </div>
-                                        {/* <div className="news">
-                                        {data.mainFooter.newsLetter.singleNews.map((item, i)=>{
-                                            return <div className="news-item" key={i}>
-                                                    <img src={item.image} className="img-fluid" alt="News" />
-                                                    <div className="overlay text-center">
-                                                        <Link to={item.link}><i className="fas fa-link"></i></Link>
-                                                    </div>
-                                                </div>
-                                                })}
-                                        </div> 
-                                    </div> */}
                                 </div>
                             </div> 
                         </section>
@@ -93,15 +43,6 @@ const Footer =()=>{
                                                 </p>
                                             </div>
                                         </div>
-                                        {/* <div className="col-lg-6 col-md-7 mt-sm-20">
-                                            <div className="footer-menu">
-                                                <ul className="d-flex justify-content-end">
-                                                    {data.footerBottom.bottomPageLink.map((item, i)=>{
-                                                        return <li key={i}><a href={item.pageLink}>{item.pageName}</a></li>
-                                                    })}
-                                                </ul>
-                                            </div>
-                                        </div> */}
                                     </div>
                                 </div>
                             </div>
